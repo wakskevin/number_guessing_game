381344cd949f2ca09d279ffe2afb9517cdf869eb
wakskevin <kevin.wasike@loxinformatics.com>
2024-05-01 12:34:26 +0300

    small improvement: initial focus on input field

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const guessField = document.querySelector(".guessField");
 let guessCount = 1;
 let resetButton;
 
+guessField.focus();
+
 function checkGuess() {
     const userGuess = Number(guessField.value);
     if (guessCount === 1) {
